65b0930ce4e4cb29b23559c2285f308559f3cd29
NsamaChibulu <nsama.chibulu@outlook.com>
2023-12-27 18:34:47 +0000

    tests

---
+++ books/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('/', views.index, name ='book.all'),
+    path('/<int:id>', views.show,name='book.show')
+]
---
+++ books/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+
+from books.models import Book
+from django.http import Http404
+
+
+def index(request):
+    dbData = Book.objects.all()
+    context = {'books': dbData}
+    return render(request,'books/index.html', context)
+
+def show(request,id):
+    singleBook = get_object_or_404(Book, pk=id)
+
+    context = {'book': singleBook}
+    return render(request,'books/show.html', context)
+
+
